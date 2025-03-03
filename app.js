4f52e5717455483498eec73d575d4dde696db4aa
Eunmi Lee <eunoia0235@gmail.com>
2025-03-04 02:53:04 +0900

    feat: 노트, 유저 라우팅에 따른 기능 추가

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const notesRouter = require('./routes/notes');
-
+// const favoritesRouter = require('./routes/favorites');
+const usersRouter = require('./routes/users');
 app.use(express.json());
 
 app.use('/notes', notesRouter);
+// app.use('/favorites', favoritesRouter);
+app.use('/', usersRouter);
 
 app.listen(3000);
